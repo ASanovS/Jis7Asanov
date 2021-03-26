a8f2cf0e683b7cca054f146d88eecc925d2a73b2
asanov <serge.asanov@gmail.com>
2021-03-26 23:15:56 +0200

    add MathOperationsDemo

---
+++ src/lesson2/level1/MathOperationsDemo.java
@@ -0,0 +1,29 @@
+package lesson2.level1;
+
+public class MathOperationsDemo {
+
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+    public void printInfoMathOperationsDemo() {
+        System.out.println("Метод addition 5 + 5 = " + addition(5, 5));
+        System.out.println("Метод subtraction 5 - 5 = " + subtraction(5, 5));
+        System.out.println("Метод multiplication 5 * 5 = " + multiplication(5, 5));
+        System.out.println("Метод division 5 / 5 = " + division(5, 5));
+
+    }
+}
--- src/run/Application.java
+++ src/run/Application.java
@@ -1,8 +1,12 @@
 package run;
 
+import lesson2.level1.MathOperationsDemo;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello JIS 7!");
+        MathOperationsDemo mathOperationsDemo = new MathOperationsDemo();
+        System.out.println("Работа математических методов");
+        mathOperationsDemo.printInfoMathOperationsDemo();
     }
 }
