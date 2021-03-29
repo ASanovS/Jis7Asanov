794fbc5e256d562a315e7a6fd5cb9cb185416d48
asanov <serge.asanov@gmail.com>
2021-03-29 23:21:06 +0300

    hw arrays 1 added print double massive

--- src/lecture2/array/HWArrays1.java
+++ src/lecture2/array/HWArrays1.java
@@ -12,22 +12,42 @@ public class HWArrays1 {
         for (int i = 0; i < mas.length; i++) {
             mas[i] = random.nextInt(100);
         }
+
         Arrays.sort(mas);
         printMas(mas);
         printMas(copyMass(mas));
+        printMas(printDoubleMas(mas, copyMass(mas)));
     }
 
     private static int[] copyMass(int[] mas) {
         int[] copyMas = new int[mas.length];
         System.arraycopy(mas, 0, copyMas, 0, mas.length);
-        for (int i = 0; i < copyMas.length - 1; i++) {
-            if (copyMas[i] < copyMas[i + 1]) {
-                int tmp = copyMas[i];
-                copyMas[i] = copyMas[i + 1];
-                copyMas[i + 1] = tmp;
+        bubbleSort(copyMas);
+        return copyMas;
+    }
+
+    private static int[] printDoubleMas(int[] mas1, int[] mas2) {
+        int[] doubleMassive = new int[mas1.length + mas2.length];
+        System.arraycopy(mas1, 0, doubleMassive, 0, mas1.length);
+        if (doubleMassive.length - mas2.length >= 0)
+            System.arraycopy(mas2, mas2.length - mas2.length, doubleMassive, mas2.length, doubleMassive.length - mas2.length);
+        return doubleMassive;
+    }
+
+    private static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int temp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    sorted = false;
+                }
             }
         }
-        return copyMas;
     }
 
     private static void printMas(int[] mas) {
