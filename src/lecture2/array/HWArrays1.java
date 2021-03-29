d719292896c486926efc13ff0dc14251c12da0fb
asanov <serge.asanov@gmail.com>
2021-03-29 22:44:58 +0300

    hw arrays 1 complete

--- src/lecture2/array/HWArrays1.java
+++ src/lecture2/array/HWArrays1.java
@@ -1,5 +1,6 @@
 package lecture2.array;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class HWArrays1 {
@@ -11,10 +12,28 @@ public class HWArrays1 {
         for (int i = 0; i < mas.length; i++) {
             mas[i] = random.nextInt(100);
         }
+        Arrays.sort(mas);
+        printMas(mas);
+        printMas(copyMass(mas));
+    }
 
+    private static int[] copyMass(int[] mas) {
+        int[] copyMas = new int[mas.length];
+        System.arraycopy(mas, 0, copyMas, 0, mas.length);
+        for (int i = 0; i < copyMas.length - 1; i++) {
+            if (copyMas[i] < copyMas[i + 1]) {
+                int tmp = copyMas[i];
+                copyMas[i] = copyMas[i + 1];
+                copyMas[i + 1] = tmp;
+            }
+        }
+        return copyMas;
+    }
+
+    private static void printMas(int[] mas) {
         for (int ma : mas) {
             System.out.printf("%s, ", ma);
         }
-
+        System.out.println();
     }
 }
