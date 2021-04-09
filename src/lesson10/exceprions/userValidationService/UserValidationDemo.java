79c277a3dc13efc7ed245e0b84ead068ae40dbd7
asanov <serge.asanov@gmail.com>
2021-04-09 23:35:34 +0300

    test validate methid in UserValidationDemo

--- src/lesson10/exceprions/userValidationService/UserValidationDemo.java
+++ src/lesson10/exceprions/userValidationService/UserValidationDemo.java
@@ -6,6 +6,9 @@ public class UserValidationDemo {
 
         User tom = new User("Tom", "Black", 30);
         User tom1 = new User("Tom1", "Black", 130);
+
+        service.validate(tom);
+        //service.validate(tom1); => this return runtime exception
     }
 }
 
