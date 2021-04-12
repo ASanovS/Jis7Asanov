a38920a4545359ca5dd950f7cfe6028d2e375cdb
asanov <serge.asanov@gmail.com>
2021-04-12 23:45:05 +0300

    task Book is done

--- src/lesson5/book/BookDemo.java
+++ src/lesson5/book/BookDemo.java
@@ -2,6 +2,14 @@ package lesson5.book;
 
 public class BookDemo {
     public static void main(String[] args) {
+        Book book1 = new Book("Author", "Book1", 200);
 
+        System.out.printf("Author - %s, book name - %s, pages - %s \n", book1.getAuthor(), book1.getName(), book1.getPages());
+
+        book1.setAuthor("Author2");
+        book1.setName("Book v 1.1");
+        book1.setPages(500);
+
+        System.out.printf("Author - %s, book name - %s, pages - %s \n", book1.getAuthor(), book1.getName(), book1.getPages());
     }
 }
